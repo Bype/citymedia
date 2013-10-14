dc6792e771cfc994d9f8241de6a00187c9bd952e
David Olivari <david@bype.org>
2013-10-14 12:31:38 +0200

    cleanup

--- public/js/flat/twitter.js
+++ public/js/flat/twitter.js
@@ -13,14 +13,14 @@ define([], function() {
 				top : -ht
 			});
 			$evmod.append($albtit);
-			var pos = 5*wt/4;
+			var pos = 5 * wt / 4;
 			$.getJSON('/twitter2json?q=' + info, function(data) {
 				$evmod.click(function(e) {
 					if (this.getAttribute("show") == 1) {
 						$(this).attr('show', 0);
 						$(this).children('.twitter').each(function(index, elt) {
 							$(elt).animate({
-								left : 5*wt/4
+								left : 5 * wt / 4
 							});
 						});
 					} else {
@@ -46,13 +46,17 @@ define([], function() {
 						$content.append($spacer);
 					}
 					var html = '<p><em>' + moment(element.created_at).fromNow() + '... </em><br/>';
-					html += element.text + '<br/><b>@' + element.user.screen_name + ' depuis ' + element.user.location + '</b></p>';
+					html += element.text + '<br/><b>@' + element.user.screen_name;
+					if (element.user.location)
+						html += ' depuis ' + element.user.location + '</b></p>';
+					else
+						html += '</b></p>';
 					$content.append(html);
 					if (element.entities.media)
 						$content.append("<br><img width='160' src='" + element.entities.media[0].media_url + "'/>");
 					$eltitem.attr('endpos', pos);
 					$eltitem.animate({
-						left : 5*wt/4
+						left : 5 * wt / 4
 					});
 					pos += 12 * wt / 2;
 					$evmod.append($eltitem);
