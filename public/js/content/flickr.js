9362f6c93c08bac260fc40384d6ad6a4aaac42f0
David Olivari <david@bype.org>
2013-11-12 17:39:11 +0100

    map link

--- public/js/content/flickr.js
+++ public/js/content/flickr.js
@@ -5,17 +5,46 @@ define(['lib/mustache', 'lib/async'], function(Mustache, async) {
 				iconHeader($elt, idx, 'flickr');
 				idx++;
 				insert($elt, idx, data.photoset.photo, 'flickr', function($div, element) {
+
 					var urlm = Mustache.render("http://farm{{farm}}.staticflickr.com/{{server}}/{{id}}_{{secret}}_b.jpg", element);
 					var $href = $(document.createElement('a'));
 					$href.addClass("fancybox");
 					$href.attr('href', urlm);
+					$href.attr('title', element.title);
 					var $img = $(document.createElement('img'));
 					var urls = Mustache.render("http://farm{{farm}}.staticflickr.com/{{server}}/{{id}}_{{secret}}_q.jpg", element);
 					$img.attr('src', urls);
+					$img.attr('alt', "");
+					//subtitle
 					$img.addClass('qimg content');
 					$href.append($img);
 					$div.append($href);
-					$href.fancybox();
+					var fb2 = $href.fancybox({
+						helpers : {
+							title : {
+								type : 'inside'
+							}
+						},
+						afterLoad : function() {
+							this.title = this.title + ' ' + $(this.element).find('img').attr('alt');
+						}
+					});
+
+					$.getJSON('http://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation&api_key=b27e622e07f348e026d868f2ee68830c&photo_id=' + element.id + '&format=json&jsoncallback=?', function(phinfo) {
+						if (phinfo.stat == 'ok') {
+							var mappos = new google.maps.LatLng(phinfo.photo.location.latitude, phinfo.photo.location.longitude);
+							var marker = new google.maps.Marker({
+								map : map,
+								position : mappos,
+								icon : '/img/camera.png'
+							});
+							google.maps.event.addListener(marker, 'click', function() {
+								$.showMap(false, function() {
+									fb2.trigger('click');
+								});
+							});
+						}
+					});
 				}, fn);
 			});
 		}
