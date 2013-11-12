d76de7151bb9621d99a662f8b69b1d29191bedf8
David Olivari <david@bype.org>
2013-11-12 17:54:40 +0100

    adding more photos

--- public/js/content/flickr.js
+++ public/js/content/flickr.js
@@ -1,7 +1,7 @@
 define(['lib/mustache', 'lib/async'], function(Mustache, async) {
 	return {
 		render : function($elt, idx, info, fn, insert, iconHeader) {
-			$.getJSON('http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=b27e622e07f348e026d868f2ee68830c&photoset_id=' + info + '&per_page=10&format=json&jsoncallback=?', function(data) {
+			$.getJSON('http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=b27e622e07f348e026d868f2ee68830c&photoset_id=' + info + '&per_page=12&format=json&jsoncallback=?', function(data) {
 				iconHeader($elt, idx, 'flickr');
 				idx++;
 				insert($elt, idx, data.photoset.photo, 'flickr', function($div, element) {
